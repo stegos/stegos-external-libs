231f4b353ceee447ba5e075ac4c5580a9d6e1354
Eugene Chupriyanov <tchu@tchu.ru>
2019-02-21 10:27:58 +0300

    Added MPFR to Linux pack

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ install_deps_linux() {
         apt-get install -y \
             bison \
             build-essential \
+            curl \
             flex \
-            libgmp-dev \
-            libmpfr-dev
+            libgmp-dev
     (cd /tmp &&
         curl -L https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2 | tar xvfj -)
     mkdir var
@@ -22,13 +22,26 @@ install_deps_macos() {
 BASE="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PBC_SRC=https://crypto.stanford.edu/pbc/files/pbc-0.5.14.tar.gz
 FLINT_SRC=http://www.flintlib.org/flint-2.5.2.tar.gz
+MPFR_SRC=https://www.mpfr.org/mpfr-current/mpfr-4.0.2.tar.gz
+
+# where make install will install stuff
+var=${BASE}/var
+prefix=${var}/local
+src=${var}/src
+pbc=${src}/pbc-0.5.14
+flint=${src}/flint-2.5.2
+mpfr=${src}/mpfr-4.0.2
+
+lib=${prefix}/lib
+inc=${prefix}/include
 
 uname_s=$(uname -s)
 case ${uname_s} in
 Linux*)
     arch=linux
     PBC_CONFIGURE_FLAGS="--enable-static"
-    FLINT_CONFIGURE_FLAGS="--enable-static --disable-shared"
+    MPFR_CONFIGURE_FLAGS="--enable-static --disable-shared"
+    FLINT_CONFIGURE_FLAGS="--enable-static --disable-shared --with-mpfr=${prefix}"
     install_deps_linux
     ;;
 Darwin*)
@@ -42,16 +55,6 @@ Darwin*)
 
     ;;
 esac
-
-# where make install will install stuff
-var=${BASE}/var
-prefix=${var}/local
-src=${var}/src
-pbc=${src}/pbc-0.5.14
-flint=${src}/flint-2.5.2
-
-lib=${prefix}/lib
-inc=${prefix}/include
 
 mkdir -p ${src}
 
@@ -67,6 +70,16 @@ cd ${src} &&
 # Use @rpath in libraries ids for macOS
 if [[ ${uname_s} == Darwin* ]]; then
     (cd ${lib} && install_name_tool -id '@rpath/libpbc.dylib' libpbc.dylib)
+fi
+
+# Build MPFR for Linux
+if [[ ${uname_s} == Linux* ]]; then
+    cd ${src} &&
+        curl -L ${MPFR_SRC} | tar xvfz - &&
+        cd ${mpfr} &&
+        ./configure ${MPFR_CONFIGURE_FLAGS} --prefix=${prefix} &&
+        make &&
+        make install
 fi
 
 # Build Flint library
