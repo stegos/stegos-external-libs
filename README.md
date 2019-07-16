ebe2c0706eae7ec7762fd743d2541b8273052215
Eugene Chupriyanov <tchu@tchu.ru>
2019-07-16 14:16:03 +0300

    Removed PBC library

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 Contains CI scripts to build and package external libraries used in [Stegos](https://github.com/stegos/stegos) application.
 
 * Libraries used:
-  * [PBC v0.5.14](https://crypto.stanford.edu/pbc/)
+  * ~~[PBC v0.5.14](https://crypto.stanford.edu/pbc/)~~ no longer used
   * [Flint 2.5.2](https://www.flintlib.org)
--- build.sh
+++ build.sh
@@ -20,7 +20,6 @@ install_deps_macos() {
 }
 
 BASE="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PBC_SRC=https://crypto.stanford.edu/pbc/files/pbc-0.5.14.tar.gz
 FLINT_SRC=http://www.flintlib.org/flint-2.5.2.tar.gz
 MPFR_SRC=https://www.mpfr.org/mpfr-current/mpfr-4.0.2.tar.gz
 
@@ -39,14 +38,12 @@ uname_s=$(uname -s)
 case ${uname_s} in
 Linux*)
     arch=linux
-    PBC_CONFIGURE_FLAGS="--enable-static"
     MPFR_CONFIGURE_FLAGS="--enable-static --disable-shared"
     FLINT_CONFIGURE_FLAGS="--enable-static --disable-shared --with-mpfr=${prefix}"
     install_deps_linux
     ;;
 Darwin*)
     arch=osx
-    PBC_CONFIGURE_FLAGS="--enable-static"
     FLINT_CONFIGURE_FLAGS="--enable-static"
     install_deps_macos
     ;;
@@ -57,15 +54,6 @@ Darwin*)
 esac
 
 mkdir -p ${src}
-
-# Build PBC library
-
-cd ${src} &&
-    curl -L ${PBC_SRC} | tar xvfz - &&
-    cd ${pbc} &&
-    ./configure ${PBC_CONFIGURE_FLAGS} --prefix=${prefix} &&
-    make &&
-    make install
 
 # Use @rpath in libraries ids for macOS
 if [[ ${uname_s} == Darwin* ]]; then
